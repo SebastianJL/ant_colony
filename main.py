318f234579afd3718f6a6b06554f1740289b7a76
SebastianJL <johannes.lade@yahoo.com>
2022-05-09 15:19:23 +0200

    Make screen size and grid drawing size adaptive.
    - Screen size is now defined relative to the actual hardware screen this is running on.
    - The grid is drawn such that at least one side expands to cover the whole screen.

--- main.py
+++ main.py
@@ -26,10 +26,14 @@ def main():
     obstacle_grid[:, 0] = True
     pheromone_grid = np.zeros((grid_width, grid_height, 2), dtype=float)
 
-    screen = pg.display.set_mode((grid_width * screensize_multiplier, grid_height * screensize_multiplier))
+    info_object = pg.display.Info()
+    SCREEN_WIDTH, SCREEN_HEIGHT = info_object.current_w//2, info_object.current_h//2
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    screen_rect = screen.get_rect()
+    grid_rect = pg.rect.Rect(0, 0, grid_width, grid_height).fit(screen_rect)
 
     ants = [Ant(grid_width//2, grid_height//2, speed=120) for _ in range(1000)]
-    prev_time = time.perf_counter()
 
     while True:
         for event in pg.event.get():
@@ -37,17 +41,36 @@ def main():
                 pg.quit()
                 exit()
 
-        screen.fill(pg.color.Color('black'))
-        screen.blit(update_fps(), (10, 0))
-
         for ant in ants:
             ant.move(obstacle_grid, pheromone_grid)
-            pg.draw.rect(screen, (0, 0, 255), pg.Rect(ant.x*screensize_multiplier,
-                                                      ant.y*screensize_multiplier,
-                                                      screensize_multiplier,
-                                                      screensize_multiplier))
+
+        draw_scene(screen, grid_rect, ants, obstacle_grid, pheromone_grid)
+
         CLOCK.tick(FPS)
         pg.display.flip()
+
+
+def draw_scene(screen, grid_rect, ants, obstacle_grid, pheromone_grid):
+    """Draw ants, obstacles and pheromones."""
+    grid_width, grid_height = obstacle_grid.shape
+    block_size = grid_rect.width//grid_width
+    top, left = grid_rect.top, grid_rect.left
+
+    # Draw background and grid background.
+    screen.fill(pg.color.Color('black'))
+    pg.draw.rect(screen, (130, 130, 130), grid_rect)
+
+    # Draw ants.
+    for ant in ants:
+        ant_rect = pg.Rect(
+            left + ant.y*block_size, top + ant.x*block_size,
+            block_size, block_size)
+        pg.draw.rect(screen, (0, 0, 255), ant_rect)
+
+    # TODO: Draw obstacles
+    # TODO: draw pheromones
+
+    screen.blit(update_fps(), (10, 0))
 
 
 class Ant:
@@ -66,21 +89,21 @@ class Ant:
 
         # Check if direction is blocked by obstacle.
         height, width = obstacle_grid.shape
-        if obstacle_grid[(self.x+1) % width, self.y]:
+        if obstacle_grid[(self.x + 1)%width, self.y]:
             possible_directions[directions.Right] = 0
-        if obstacle_grid[(self.x-1), self.y]:
+        if obstacle_grid[(self.x - 1), self.y]:
             possible_directions[directions.Left] = 0
-        if obstacle_grid[self.x, (self.y+1) % height]:
+        if obstacle_grid[self.x, (self.y + 1)%height]:
             possible_directions[directions.Up] = 0
-        if obstacle_grid[self.x, self.y-1]:
+        if obstacle_grid[self.x, self.y - 1]:
             possible_directions[directions.Down] = 0
 
         # Weigh direction by self.direction.
         # Todo: Maybe remove? -> current implementation causes NaN values
-        #possible_directions[self.direction.value] *= 0.5
-        #possible_directions[(self.direction.value + 1) % 4] *= 0.2
-        #possible_directions[(self.direction.value - 1) % 4] *= 0.2
-        #possible_directions[(self.direction.value + 2) % 4] *= 0.1
+        # possible_directions[self.direction.value] *= 0.5
+        # possible_directions[(self.direction.value + 1) % 4] *= 0.2
+        # possible_directions[(self.direction.value - 1) % 4] *= 0.2
+        # possible_directions[(self.direction.value + 2) % 4] *= 0.1
 
         # Weigh direction by pheromone.
         # Todo: implement
@@ -90,13 +113,13 @@ class Ant:
 
         # Move
         if self.direction == directions.Right:
-            self.x = (self.x + 1) % width
+            self.x = (self.x + 1)%width
         elif self.direction == directions.Up:
-            self.y = (self.y + 1) % height
+            self.y = (self.y + 1)%height
         elif self.direction == directions.Left:
-            self.x = (self.x - 1) % width
+            self.x = (self.x - 1)%width
         elif self.direction == directions.Down:
-            self.y = (self.y - 1) % height
+            self.y = (self.y - 1)%height
 
 
 if __name__ == '__main__':
